80d9dc97a301efd603f687e362ad6a5aedf5aaa5
Alin Dondera <a.dondera@student.tudelft.nl>
2020-05-05 22:22:59 +0200

    add app.py to run server

--- api/__init__.py
+++ api/__init__.py
@@ -6,10 +6,10 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    # )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
