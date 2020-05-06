75614e2547c7f0eb890538a46fb57b891520100f
amanolache <a.d.manolache@student.tudelft.nl>
2020-05-06 20:17:40 +0200

    read from data

--- api/routes.py
+++ api/routes.py
@@ -3,8 +3,32 @@ from api.models import User
 from api import db
 from flask.json import jsonify
 from flask import request
+from typing import List
 
-## Define error messages
+
+def read_form_data(request, file_keys: List[str]=[]) -> dict:
+    """Returns the request's form data as a dictionary, both in `request.form`
+       and `request.files`. Importantly, it also ensures through the file_keys
+       list that form data expected to be sent as a file, does not appear as
+       a form key-value pair.
+    """
+    data = {}
+    if request.json:
+        data.update(request.json)
+    if request.form.to_dict():
+        data.update(request.form.to_dict())
+
+    # for file_key in file_keys:
+    #     data.pop(file_key, None) # should not be there as form val
+
+    data.update(request.files.to_dict())
+    # if bool(data):
+    #     log_general_info(data)
+    #     return data
+    return None
+
+
+# Define error messages
 ANSWERS = { 200: "200 OK",
             201: "201 Created",
             204: "204 No Content",
@@ -27,16 +51,20 @@ def index():
 
 
 @app.route('/form', methods=['POST'])
-def form(obj):
+def form():
     if request.method == 'POST':
-        print(obj)
+        data = read_form_data(request)
+
+        print("GOT DATA:")
+        print(data)
+
         return jsonify(ANSWERS[200]), 200
 
     return jsonify(ANSWERS[400]), 400
 
 
 @app.route('/test/<first>/<second>')
-def databasetest(first=None, second=None):
+def database_test(first=None, second=None):
     me = User(first, second)
     db.session.add(me)
     db.session.commit()
