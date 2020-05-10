0ef79cb7642d561025ffa6ef942a8a44de33293e
Alin Dondera <a.dondera@student.tudelft.nl>
2020-05-10 15:45:11 +0200

    change schema

--- api/models.py
+++ api/models.py
@@ -6,11 +6,11 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, password=None):
         self.username = name
-        self.email = email
+        self.password = password
 
     def __repr__(self):
         return '<User %r>' % self.username
