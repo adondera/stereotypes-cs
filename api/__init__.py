40333621321f5abb8cf589bde8e0948ccd4ae9cf
Alin Dondera <a.dondera@student.tudelft.nl>
2020-05-12 18:39:59 +0200

    add flask migrate

--- api/__init__.py
+++ api/__init__.py
@@ -4,25 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
-### Flask setup
+# Flask setup
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-### Enables CORS
+# Enables CORS
 cors = CORS(app)
 
-### Bcrypt setup
+# Bcrypt setup
 bcrypt = Bcrypt(app)
 
-
-### JSON Access Token setup
+# JSON Access Token setup
 jwt = JWTManager(app)
 
-
-### Database setup
+# Database setup
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % app.config['POSTGRES']
