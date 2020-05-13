0b22d157e7d3d1e88fd3347d29742109ea6d4d8d
Dragos <dvecerdea@student.tudelft.nl>
2020-05-13 15:55:33 +0200

    try to configure ci for front-end

--- client/data-collection/src/App.test.js
+++ client/data-collection/src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-
+import Information from '../src/components/QuestionTypes/Information'
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<Information/>);
 });
