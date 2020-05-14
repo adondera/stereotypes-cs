300dedffaa3fb854375c050a25d318fc1601c511
Dragos <dvecerdea@student.tudelft.nl>
2020-05-14 13:29:54 +0200

    install likert scale component after npm install

--- client/data-collection/src/App.test.js
+++ client/data-collection/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Information from '../src/components/QuestionTypes/Information'
-import LikertScaleQuestion from '../src/components/QuestionTypes/LikertScaleQuestion';
+import LikertScaleQuestion from '../src/components/QuestionTypes/LikertScaleQuestion.js';
 test('renders learn react link', () => {
   render(<LikertScaleQuestion></LikertScaleQuestion>)
   const { getByText } = render(<Information/>);
