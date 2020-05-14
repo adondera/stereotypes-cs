4684cce3178af931f066b28f4dc61d41e93af1b2
Dragos <dvecerdea@student.tudelft.nl>
2020-05-14 12:57:26 +0200

    remove app component from test

--- client/data-collection/src/App.test.js
+++ client/data-collection/src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 import Information from '../src/components/QuestionTypes/Information'
 test('renders learn react link', () => {
   const { getByText } = render(<Information/>);
