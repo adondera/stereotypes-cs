b26f545bbadb7a9abc4e7ba9c5b85503d6c1d242
Alin Dondera <a.dondera@student.tudelft.nl>
2020-05-18 16:39:04 +0200

    add consent form back

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 from flask_restful import Api
 from .auth import Login, FreshLogin
 from .tokens import Protected, Refresh, ProtectedFresh
-from .consent import ConsentResource
+from .consent import ConsentResource, ConsentForm
 
 bp = Blueprint("endpoint", __name__)
 
@@ -17,3 +17,4 @@ api.add_resource(Refresh, '/refresh')
 api.add_resource(FreshLogin, '/fresh-login')
 api.add_resource(ProtectedFresh, '/protected-fresh')
 api.add_resource(ConsentResource, '/submit')
+api.add_resource(ConsentForm, '/form')
