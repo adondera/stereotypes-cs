8592195c5e29e5efb22048403bbb2f037552170c
Dragos <dvecerdea@student.tudelft.nl>
2020-05-19 15:14:04 +0200

    changenged to secure url

--- client/data-collection/src/utils/API.js
+++ client/data-collection/src/utils/API.js
@@ -7,7 +7,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://nemo-live-science-dev.herokuapp.com",
+  baseURL: "https://nemo-live-science-dev.herokuapp.com",
   responseType: "json",
 });
 
