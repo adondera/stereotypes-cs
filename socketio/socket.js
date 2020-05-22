5de2c36302de955e105b7eea6d7806fb51aa4260
Alin Dondera <a.dondera@student.tudelft.nl>
2020-05-22 21:43:20 +0200

    fix https redirection

--- socketio/socket.js
+++ socketio/socket.js
@@ -1,9 +1,10 @@
 
 
-let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTAxNjE4MzgsIm5iZiI6MTU5MDE2MTgzOCwianRpIjoiN2Q4OWZhM2ItZWU1YS00MmM0LTk1YTYtNGUxMjAzMDk0OWRiIiwiZXhwIjoxNTkwMTYyNzM4LCJpZGVudGl0eSI6ImFkbWluIiwiZnJlc2giOnRydWUsInR5cGUiOiJhY2Nlc3MifQ.6bj4aLgjXjcnq_BKOoN9IMaX8tNo55YnzVFtoG1kbNg'
+let token = 
+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTAxNzQyOTQsIm5iZiI6MTU5MDE3NDI5NCwianRpIjoiNzc4ZTRlZjctN2I3Mi00ZGUyLWI0MzQtMmE1N2UyMDg5ZmIyIiwiZXhwIjoxNTkwMTc1MTk0LCJpZGVudGl0eSI6ImFkbWluIiwiZnJlc2giOnRydWUsInR5cGUiOiJhY2Nlc3MifQ.q6aAwEe0u0VY2a0zDEPDDl7R9wlDf6i7aqEyRoZLZC8"
 const io = require('socket.io-client');
 
-let socket = io("http://localhost:5000", {extraHeaders: { 'Authorization': `Bearer ${token}` }});
+let socket = io("wss://nemo-live-science-dev.herokuapp.com/", {extraHeaders: { 'Authorization': `Bearer ${token}` }});
 let free = true
 let readline = require('readline').createInterface({
   input: process.stdin,
