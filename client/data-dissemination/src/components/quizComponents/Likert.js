abedc01c91682ea54f7ea6d1aff068e393152348
Dragos <dvecerdea@student.tudelft.nl>
2020-05-28 22:46:37 +0200

    likert register answer to store

--- client/data-dissemination/src/components/quizComponents/Likert.js
+++ client/data-dissemination/src/components/quizComponents/Likert.js
@@ -1,75 +1,98 @@
-import React from 'react';
-import Likert from 'react-likert-scale'
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Slide from '@material-ui/core/Slide'
-import Paper from '@material-ui/core/Paper';
-import {Link} from 'react-router-dom';
-
+import React, { useState } from "react";
+import Likert from "react-likert-scale";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Slide from "@material-ui/core/Slide";
+import Paper from "@material-ui/core/Paper";
+import { Link } from "react-router-dom";
+import "../../styles/Likert.css";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '95%',
-        margin: 'auto',
-        flexGrow: 1,
-        marginTop: '20px',
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
+  root: {
+    width: "95%",
+    margin: "auto",
+    flexGrow: 1,
+    marginTop: "20px",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
 }));
 
 const LikertScaleQuestion = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [picked, setpicked] = useState(false);
 
-    const likertOptions = {
-        responses: [{value: 1, text: 'hello'}, {value: 2, text: 'world'}, {value: 3, text: 'this'}, {
-            value: 4,
-            text: 'was'
-        }, {value: 5, text: 'Andrei'}],
-        picked: 1
-    };
+  const onClick = () => {
+    const answer = { value: picked, id: props.id };
+    props.registerAnswer(answer);
+    props.onNext();
+  };
+  const likertOptions = {
+    responses: [
+      { value: 1, text: "Strongly disagree" },
+      { value: 2, text: "Disagree" },
+      { value: 3, text: "Neutral" },
+      { value: 4, text: "Agree" },
+      { value: 5, text: "Strongly agree" },
+    ],
+    picked: (value) => setpicked(value),
+  };
 
-
-    return (
-        <React.Fragment>
-            <div className={classes.root}>
-                <Grid container spacing={3} alignItems='center' justify='center'>
-                    <Grid item xs={12}>
-                        <Slide direction='down' in={true} key={props.questionIndex} mountOnEnter>
-                            <Card className={classes.root}>
-                                <CardContent>
-                                    <Typography gutterBottom variant='h5' component='h2'>
-                                        {props.text}
-                                    </Typography>
-                                    <Likert key={props.questionIndex} {...likertOptions} className='likertScale'/>
-                                </CardContent>
-                            </Card>
-                        </Slide>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <Link to='/quiz' style={{textDecoration: 'none'}}>
-                                <Button
-                                    style={{margin: 'auto'}}
-                                    variant='contained'
-                                    color='primary'
-                                    onClick={props.onNext}>
-                                    NEXT
-                                </Button>
-                            </Link>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div className={classes.root}>
+        <Grid container spacing={3} alignItems="center" justify="center">
+          <Grid item xs={12}>
+            <Slide
+              direction="down"
+              in={true}
+              key={props.questionIndex}
+              mountOnEnter
+            >
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography
+                    style={{ textAlign: "center" }}
+                    variant="h5"
+                    component="h2"
+                  >
+                    {props.text}
+                  </Typography>
+                  <Likert
+                    key={props.questionIndex}
+                    {...likertOptions}
+                    className="likertScale"
+                  />
+                </CardContent>
+              </Card>
+            </Slide>
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <Paper className={classes.paper} elevation={0}>
+              <Link to="/quiz" style={{ textDecoration: "none" }}>
+                <Button
+                  style={{ margin: "auto" }}
+                  variant="contained"
+                  color="primary"
+                  disabled={!picked}
+                  onClick={onClick}
+                >
+                  NEXT
+                </Button>
+              </Link>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default LikertScaleQuestion;
--- client/data-dissemination/src/constants/store.js
+++ client/data-dissemination/src/constants/store.js
@@ -5,32 +5,33 @@ const initialStore = {
   appReducer: {
     quizIndex: 0,
     quizData: [
+      // {
+      //   id: 69,
+      //   type: 1,
+      //   title: 'Binary question',
+      //   textLeft: 'Programmer',
+      //   textRight: 'Writer',
+      //   image: 'https://i.imgur.com/9GIFW9f.jpg',
+      //   correctAnswer: 'right',
+      // },
+      // {
+      //   id: 42,
+      //   type: 1,
+      //   title: 'Binary question ',
+      //   textLeft: 'Pula',
+      //   textRight: 'Pizda',
+      //   image: 'https://i.imgur.com/9GIFW9f.jpg',
+      //   correctAnswer: 'left',
+      // },
+      // {
+      //   type: 4,
+      //   title: 'Information',
+      //   header: 'Gender profession IAT',
+      //   body:
+      //     'In the following minutes you will see several images with two words each. One on the right, and the other on the left. Please choose the word you consider aproppriate: Left word (key E), Right word (key I). Please click on Next when ready',
+      // },
       {
-        id: 69,
-        type: 1,
-        title: 'Binary question',
-        textLeft: 'Programmer',
-        textRight: 'Writer',
-        image: 'https://i.imgur.com/9GIFW9f.jpg',
-        correctAnswer: 'right',
-      },
-      {
-        id: 42,
-        type: 1,
-        title: 'Binary question ',
-        textLeft: 'Pula',
-        textRight: 'Pizda',
-        image: 'https://i.imgur.com/9GIFW9f.jpg',
-        correctAnswer: 'left',
-      },
-      {
-        type: 4,
-        title: 'Information',
-        header: 'Gender profession IAT',
-        body:
-          'In the following minutes you will see several images with two words each. One on the right, and the other on the left. Please choose the word you consider aproppriate: Left word (key E), Right word (key I). Please click on Next when ready',
-      },
-      {
+        id: 96,
         type: 2,
         title: 'Likert Scale',
         text: 'Programming is a profession for men',
