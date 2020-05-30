63abbb052748d1a5dcc7b3a2459a920150c67493
Dragos <dvecerdea@student.tudelft.nl>
2020-05-30 16:36:35 +0200

    style end

--- client/data-collection/src/components/QuestionTypes/Finish.js
+++ client/data-collection/src/components/QuestionTypes/Finish.js
@@ -3,8 +3,64 @@ import { clearQuestionsStore, sendQuestionsAnswers } from "../../actions/questio
 import {connect} from "react-redux"
 import Button from "@material-ui/core/Button";
 import { Typography } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+
+
 const Finish = (props) => {
 
+  const classes = useStyles()
+  
   //sequence of actions to be dispatched whne quiz ends
   const onClick = () => {
     props.sendQuestionsAnswers(props.childId);
@@ -13,14 +69,26 @@ const Finish = (props) => {
   };
   
   return (
-    <div>
-      <Typography variant="h5" style={{textAlign: "justify"}}>
+    <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="sm" component="main" className={classes.heroContent}>
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="textPrimary"
+        gutterBottom
+      >
+        {props.title}
+      </Typography>
+      <Typography variant="h5" align="center" color="textSecondary" component="p">
         {props.text}
       </Typography>
-      <Button style={{marginTop: 20}}variant="contained" onClick={onClick}>
+    </Container>
+    <Button style={{marginTop: 20}}variant="contained" onClick={onClick}>
         END
       </Button>
-    </div>
+  </React.Fragment>
   );
 };
 
