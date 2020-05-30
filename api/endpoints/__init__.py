9e5634c6d866b0eb11a24f057122baf8f448db88
Alin Dondera <a.dondera@student.tudelft.nl>
2020-05-30 18:57:49 +0200

    add endpoint

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 from .auth import Login, FreshLogin
 from .tokens import Protected, Refresh, ProtectedFresh, Unprotected
 from .consent import ConsentResource, ConsentForm
-from .quiz import QuizAnswers, QuizQuestions
+from .quiz import QuizAnswers, QuizQuestions, QuizResults
 
 bp = Blueprint("endpoint", __name__)
 
@@ -22,3 +22,4 @@ api.add_resource(ConsentForm, '/form')
 api.add_resource(Unprotected, '/unprotected')
 api.add_resource(QuizQuestions, '/quiz')
 api.add_resource(QuizAnswers, '/answers')
+api.add_resource(QuizResults, '/results')
