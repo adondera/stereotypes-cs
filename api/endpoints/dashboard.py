37f56cef2a4a6794e57ed8be4208ef2e1a839551
ionut <i.l.constantinescu@student.tudelft.nl>
2020-06-03 23:21:55 +0200

    document dashoboard module

--- api/endpoints/dashboard.py
+++ api/endpoints/dashboard.py
@@ -1,4 +1,5 @@
 # pylint: disable=no-self-use
+"""Module that is used for sending data that can be shown on the admin dashboard"""
 from datetime import datetime, timedelta
 
 from flask_jwt_extended import jwt_required
@@ -9,15 +10,42 @@ from api.models import Participant, Version, Ethnicity
 
 
 def all_time_participants():
+    """
+    Get the total number of persons that have taken the test.
+
+    Returns
+    -------
+    int
+        The total number of participants.
+    """
+
     return Participant.query.count()
 
 
 def today_participants():
+    """
+    Get the number of persons that have taken the test today.
+
+    Returns
+    -------
+    int
+        The number of participants who took the test today.
+    """
+
     today = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
     return Participant.query.filter(Participant.date >= today).count()
 
 
 def yesterday_participants():
+    """
+    Get the number of persons that have taken the test yesterday.
+
+    Returns
+    -------
+    int
+        The number of participants who took the test yesterday.
+    """
+
     date = datetime.today() - timedelta(days=1)
     yesterday = datetime(date.year, date.month, date.day)
     today = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
@@ -26,26 +54,55 @@ def yesterday_participants():
 
 
 def yesterday_by_this_time_participants():
+    """
+    Get the number of persons that have taken the test until this time yesterday.
+
+    Returns
+    -------
+    int
+        The number of participants who took the test yesterday by this time.
+    """
+
     yesterday_this_time = datetime.today() - timedelta(days=1)
-    yesterday_00 = datetime(yesterday_this_time.year, yesterday_this_time.month, yesterday_this_time.day)
+    yesterday_00 = datetime(yesterday_this_time.year,
+                            yesterday_this_time.month,
+                            yesterday_this_time.day)
 
     return Participant.query.filter(and_(Participant.date >= yesterday_00),
                                     Participant.date <= yesterday_this_time).count()
 
 
 def last_hour_participants():
+    """
+    Get the number of persons that have taken the test in the last hour.
+
+    Returns
+    -------
+    int
+        The number of participants who took the test in the last hour.
+    """
+
     last_hour = datetime.today() - timedelta(hours=1)
     return Participant.query.filter(Participant.date >= last_hour).count()
 
 
 def version_distribution():
+    """
+    Get the number of participants for each version of the test.
+
+    Returns
+    -------
+    JSON
+        The version name and number of persons who had that version.
+    """
+
     data = []
     for version in Version:
-        num_participants = Participant.query.filter(Participant.quiz_version == version.name).count()
+        num_part = Participant.query.filter(Participant.quiz_version == version.name).count()
 
         version_obj = {
             'version_name': version.name,
-            'num_participants': num_participants
+            'num_participants': num_part
         }
 
         data.append(version_obj)
@@ -53,6 +110,15 @@ def version_distribution():
 
 
 def gender_distribution():
+    """
+    Get the distribution of participants per gender.
+
+    Returns
+    -------
+    JSON
+        The gender and number of persons who have this gender.
+    """
+
     data = []
     results = Participant.query.with_entities(Participant.gender, func.count(Participant.gender)) \
         .group_by(Participant.gender).all()
@@ -73,6 +139,16 @@ def gender_distribution():
 
 
 def ethnicity_distribution():
+    """
+    Get the distribution of participants per ethnicity.
+    If a person has multiple ethnicities they will be counted for each individual ethnicity.
+
+    Returns
+    -------
+    JSON
+       The ethnicity and number of persons who have this ethnicity.
+    """
+
     data = []
 
     data = []
@@ -88,14 +164,28 @@ def ethnicity_distribution():
 
 
 def avg_age():
+    """
+    Get the average age of the persons who have taken the test.
+
+    Returns
+    -------
+    int
+       The average age amongst the participants.
+    """
+
     avg = Participant.query.with_entities(func.avg(Participant.age)).one()[0]
     return float(avg)
 
 
 class Stats(Resource):
+    """Resource that deals with retrieving statistics about participants"""
 
     @jwt_required
     def get(self):
+        """
+        On a get request on the /stats endpoint we return an object with statistics
+        :return: If the request is valid, the statistics JSON object and code 200
+        """
 
         data = {
 
