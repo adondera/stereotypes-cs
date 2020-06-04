80f0cec05faf4a8d27ccc9b03035f20b91171072
Dragos <dvecerdea@student.tudelft.nl>
2020-06-04 22:53:23 +0200

    add open question to store

--- client/data-collection/src/components/QuestionTypes/OpenQuestion.js
+++ client/data-collection/src/components/QuestionTypes/OpenQuestion.js
@@ -3,24 +3,24 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core";
-
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
+  "@global": {
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
+      listStyle: "none",
     },
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
   toolbar: {
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
   },
   toolbarTitle: {
     flexGrow: 1,
@@ -33,14 +33,14 @@ const useStyles = makeStyles((theme) => ({
   },
   cardHeader: {
     backgroundColor:
-      theme.palette.type === 'light'
+      theme.palette.type === "light"
         ? theme.palette.grey[200]
         : theme.palette.grey[700],
   },
   cardPricing: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
     marginBottom: theme.spacing(2),
   },
   footer: {
@@ -48,74 +48,81 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       paddingTop: theme.spacing(6),
       paddingBottom: theme.spacing(6),
     },
   },
 }));
 
-
 const OpenQuestion = (props) => {
   /*eslint-disable */
-  const classes = useStyles()
+  const classes = useStyles();
+  const textRef = React.createRef()
 
-
-
+  const onClick = () => {
+    var answer = {}
+    answer.question_id = props.id
+    answer.open_answer = textRef.current.value
+    props.submitAnswer(answer)
+    props.onNext()
+  }
+  
   return (
     <React.Fragment>
       <CssBaseline />
-        {startsIn ? (  
-          <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          marginTop='50%'
-          color="textPrimary"
-          gutterBottom
-        >{startsIn}</Typography>) : (
-          <React.Fragment>
-        <Container maxWidth="sm" component="main" className={classes.heroContent}>      
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
         <Typography
-          component="h1"
-          variant="h2"
+          variant="h5"
           align="center"
-          color="textPrimary"
-          gutterBottom
+          color="textSecondary"
+          component="p"
         >
-          {props.header}
-        </Typography>
-        <Typography variant="h5" align="center" color="textSecondary" component="p">
           {props.text}
         </Typography>
       </Container>
       <Grid item xs={12} sm={12}>
-      <TextField
+        <TextField
           id="outlined-multiline-static"
           label="Antwoord hier"
           multiline
           rows={4}
+          inputRef={textRef}
           autoFocus
           defaultValue=""
           variant="outlined"
         />
       </Grid>
       <Grid>
-      <Button
-      style={{margin: 'auto'}}
-        variant="contained"
-        style={{ marginTop: 20 }}
-        onClick={props.onNext}
-        disabled={hasNext}
-      >
-        NEXT
-      </Button>
+        <Button
+          style={{ margin: "auto" }}
+          variant="contained"
+          style={{ marginTop: 20 }}
+          onClick={onClick}
+          disabled={hasNext}
+        >
+          NEXT
+        </Button>
       </Grid>
-      </React.Fragment>
-        )
-        }
     </React.Fragment>
   );
 };
 
-export default OpenQuestion;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...ownProps,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitAnswer: (answer) => {
+      dispatch(saveQuestionAction(answer));
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OpenQuestion);
