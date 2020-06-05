f1d754305d9d3c19fb6463ef5c6fb5570923daf5
Dragos <dvecerdea@student.tudelft.nl>
2020-06-05 14:43:17 +0200

    fix development setting

--- client/data-collection/src/reducers/mainApp.js
+++ client/data-collection/src/reducers/mainApp.js
@@ -19,8 +19,8 @@ const mainAppReducer = (
     case 'CHANGE_QUESTION':
       return {
         ...state,
-       //questionIndex: action.questionIndex,
-       questionIndex: 37
+       questionIndex: action.questionIndex,
+       //questionIndex: 37
       };
 
     /*
