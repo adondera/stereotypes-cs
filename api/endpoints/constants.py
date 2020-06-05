97de38f625d0298c27a88ae2104d75e86bbbf3a4
Alin Dondera <a.dondera@student.tudelft.nl>
2020-06-05 12:00:24 +0200

    add een

--- api/endpoints/constants.py
+++ api/endpoints/constants.py
@@ -20,46 +20,46 @@ block_start_text = [
                  "snel mogelijk en probeer zo min mogelijk fouten te maken. Mocht je toch een foutje maken, geen "
                  "probleem! Je mag het dan nog een keer proberen.",
 
-        "text1": "Hieronder zie je plaatjes van {}. Deze ga je zo naar links verplaatsen. Dit doe je door op "
+        "text1": "Hieronder zie je plaatjes van een {}. Deze ga je zo naar links verplaatsen. Dit doe je door op "
                  "de ‘e’ te tikken op het toetsenbord.",
                     
-        "text2": "Ook ga je plaatjes zien van {}. Deze ga je zo naar rechts verplaatsen. Dit doe je door op "
+        "text2": "Ook ga je plaatjes zien van een {}. Deze ga je zo naar rechts verplaatsen. Dit doe je door op "
                  "de ‘i’ te tikken op het toetsenbord."
     },
 
     {   "text0": "Goed gedaan! We gaan het nog een keer doen, maar nu met nieuwe plaatjes.",
     
-        "text1": "Hieronder zie je plaatjes van {}. Deze ga je zo naar links verplaatsen. Dit doe je door op "
+        "text1": "Hieronder zie je plaatjes van een {}. Deze ga je zo naar links verplaatsen. Dit doe je door op "
                  "de ‘e’ te tikken op het toetsenbord.",
                     
-        "text2": "Ook ga je plaatjes zien van {}. Deze ga je zo naar rechts verplaatsen. Dit doe je door op "
+        "text2": "Ook ga je plaatjes zien van een {}. Deze ga je zo naar rechts verplaatsen. Dit doe je door op "
                  "de ‘i’ te tikken op het toetsenbord."
     },
     
     {   "text0": "Nu gaan we plaatjes combineren.",
     
-        "text1": "Wanneer je een plaatje ziet van {} of {} verplaats je deze naar links door op de ‘e’ "
+        "text1": "Wanneer je een plaatje ziet van een {} of {} verplaats je deze naar links door op de ‘e’ "
                  "te tikken op het toetsenbord. Dat zijn dus deze plaatjes.",
                     
-        "text2": "Wanneer je een plaatje ziet van {} of {} verplaats je deze naar rechts door op de "
+        "text2": "Wanneer je een plaatje ziet van een {} of {} verplaats je deze naar rechts door op de "
                  "‘i’ te tikken op het toetsenbord. Dat zijn dus deze plaatjes."
     },
     
     {   "text0": "Goed gedaan! We gaan het nog een keer doen, maar draaien nu links en rechts om.",
     
-        "text1": "Hieronder zie je plaatjes van {}. Deze ga je zo naar links verplaatsen. Dit doe je door op de "
+        "text1": "Hieronder zie je plaatjes van een {}. Deze ga je zo naar links verplaatsen. Dit doe je door op de "
                  "‘e’ te tikken op het toetsenbord.",
                     
-        "text2": "Ook ga je plaatjes zien van {}. Deze ga je zo naar rechts verplaatsen. Dit doe je door op de "
+        "text2": "Ook ga je plaatjes zien van een {}. Deze ga je zo naar rechts verplaatsen. Dit doe je door op de "
                  "‘i’ te tikken op het toetsenbord."
     },
 
     {   "text0": "Nog een set met plaatjes te gaan! Je doet het heel goed.",
     
-        "text1": "Wanneer je een plaatje ziet van {} of {} verplaats je deze naar links door op de ‘e’ "
+        "text1": "Wanneer je een plaatje ziet van een {} of {} verplaats je deze naar links door op de ‘e’ "
                  "te tikken op het toetsenbord. Dat zijn dus deze plaatjes.",
                     
-        "text2": "Wanneer je een plaatje ziet van {} of {} verplaats je deze naar rechts door op de "
+        "text2": "Wanneer je een plaatje ziet van een {} of {} verplaats je deze naar rechts door op de "
                  "‘i’ te tikken op het toetsenbord. Dat zijn dus deze plaatjes."
     },
 ]
