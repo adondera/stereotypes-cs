49b3f82061cf25ac72874e27819e0efce6b0651a
Dragos <dvecerdea@student.tudelft.nl>
2020-06-20 14:10:00 +0200

    restyle results

--- client/data-dissemination/src/components/Results.js
+++ client/data-dissemination/src/components/Results.js
@@ -3,27 +3,33 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { useStyles } from "../styles/Information";
+import {
+    responsiveFontSizes,
+    ThemeProvider,
+    createMuiTheme,
+  } from "@material-ui/core/styles";
+  //responsive font sizes
+  let theme = createMuiTheme();
+  theme = responsiveFontSizes(theme);
 
 const Results = (props) => {
     const classes = useStyles();
     console.log("results_available:" + props.resultsAvailable.toString());
 
     return (
-        <div>
-            <Grid container spacing={3} alignItems="center" justify="center">
+        <ThemeProvider theme={theme}>
+            <Grid container spacing={3} alignItems="center" justify="center" style={{marginTop: 30}}>
                 {/*result*/}
-                <Grid item xs={12} sm={6}   direction="column" alignItems="center" justify="center">
+                <Grid item xs={12} sm={9} >
                     <Paper className={classes.paper} elevation={1}>
-                        <Typography variant="h4">
-                            <h1>{props.result}</h1>
+                        <Typography variant="h3">
+                            {props.result}
                         </Typography>
                     </Paper>
-                    <br/>
-                    <br/>
-                    {/*other information*/}
-                    <Paper className={classes.paper} elevation={0}>
-                        <Typography style={{ textAlign: "justify" }} variant="h6">
-                            <p>
+                </Grid>
+                {/*other information*/}
+                <Grid item xs={9} sm={9} > 
+                        <Typography style={{ textAlign: "justify" }} variant="body1">
                                 Het idee dat STEM (Science, Technology, Engineering en
                                 Mathematics) niks voor meisjes is, is een boodschap die kinderen
                                 al vroeg meekrijgen. Soms expliciet, maar vaker impliciet
@@ -31,18 +37,22 @@ const Results = (props) => {
                                 docenten. Genderstereotiepe ideeën zijn onbewust en juist
                                 daardoor zo moeilijk te doorbreken. Tips om deze associatie toch
                                 te doorbreken zijn:
-                            </p>
-                            <ul>
+                          </Typography>
+                </Grid>
+                <Grid item xs={9} sm={9}>
+                <ul>
                                 <li>
                                     Herken en erken je eigen genderstereotiepe beelden en bestrijd
                                     ze actief. Zo kun je voorkomen dat ze jouw gedrag en ideeën,
                                     en die van anderen, beïnvloeden.
                                 </li>
+                                <br/>
                                 <li>
                                     Herken de stereotiepe ideeën die meisjes en jongens hebben met
                                     betrekken tot gender en STEM, ontkracht deze beelden actief en
                                     bied tegenvoorbeelden aan.
                                 </li>
+                                <br/>
                                 <li>
                                     Wees je ervan bewust dat jouw eigen (on)zekerheid ten aanzien
                                     van STEM de interesse en het (keuze)gedrag van meisjes in jouw
@@ -51,13 +61,11 @@ const Results = (props) => {
                             </ul>
                             <p>
                                 Wilt u meer weten over gender en STEM? Bekijk dan het onderzoek
-                                van [VHTO](https://www.vhto.nl/cijfers-onderzoek/onderzoek/).
+                                van <a href='https://www.vhto.nl/cijfers-onderzoek/onderzoek/'> [VHTO] </a>
                             </p>
-                        </Typography>
-                    </Paper>
                 </Grid>
             </Grid>
-        </div>
+        </ThemeProvider>
     );
 };
 
