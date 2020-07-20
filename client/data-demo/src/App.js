04b53d9759f127c6480a605655f38f3aa37f74fd
Shirley de Wit <shirleydewit@MacBook-Pro-Pink.local>
2020-07-20 21:10:39 +0200

    trying stuff to get a demonstration application

--- client/data-demo/src/App.js
+++ client/data-demo/src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
 import "./App.css";
-import MainApp from "./containers/MainApp";
-import { connect } from "react-redux";
-import { BrowserRouter, Redirect } from "react-router-dom";
+import Home from './components/Home';
+import {connect } from "react-redux";
 import PropTypes from 'prop-types'
-
-const noSelect = {
-  WebkitUserSelect: "none", userSelect: 'none', WebkitTouchCallout: 'none'
-}
 
 const App = () => {
   return (
-    <div className="App" style={{...noSelect}}>
-        <BrowserRouter>
-          <Redirect exact to="/load" />
-          <MainApp /> 
-        </BrowserRouter>
-    </div>
+    <Route exact path='/'>
+      <Home />
+    </Route>
   );
 };
-
-const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.loginReducer.isLoggedIn,
-  };
-};
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool
-}
-
-export default connect(mapStateToProps)(App);
