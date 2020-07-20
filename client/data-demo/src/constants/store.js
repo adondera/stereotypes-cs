ef5b9db4335d780ab132c966c58cd2bc7debaf6c
Shirley de Wit <shirleydewit@MacBook-Pro-Pink.local>
2020-07-20 22:30:51 +0200

    small change to store.js

--- client/data-demo/src/constants/store.js
+++ client/data-demo/src/constants/store.js
@@ -5,25 +5,6 @@ const initialStore = {
   appReducer: {
     quizIndex: 0,
     quizData: [
-        // {
-        //     "text0": "Je gaat zo verschillende plaatjes zien die je naar links of naar rechts moet verplaatsen. Doe dit zo snel mogelijk en probeer zo min mogelijk fouten te maken. Mocht je toch een foutje maken, geen probleem! Je mag het dan nog een keer proberen.",
-        //     "text1": "Hieronder zie je plaatjes van een programmeur. Deze ga je zo naar links verplaatsen. Dit doe je door op de ‘e’ te tikken op het toetsenbord.",
-        //     "text2": "Ook ga je plaatjes zien van een schrijver. Deze ga je zo naar rechts verplaatsen. Dit doe je door op de ‘i’ te tikken op het toetsenbord.",
-        //     "q_type": "binary_information",
-        //     "text3": "Zet je linker wijsvinger op de ‘e’, en je rechter wijsvinger op de ‘i’, zo kun je snel plaatjes verplaatsen! Ben je er klaar voor? Druk dan op volgende.",
-        //     "images0": [
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276505/Profession/profession_programmer_1_c6pvby.png",
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_programmer_2_rbcfov.png",
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_programmer_3_cjwqsw.png",
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_programmer_4_xbviey.png"
-        //     ],
-        //     "images1": [
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_writer_1_o8xzue.png",
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_writer_2_mmpdnx.png",
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276505/Profession/profession_writer_3_qyl8aq.png",
-        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276505/Profession/profession_writer_4_tbvw0b.png"
-        //     ]
-        // },
         {
             "is_active":true,
             "text":"",
@@ -70,6 +51,25 @@ const initialStore = {
                 "category":"Groente"
             }
         },
+        // {
+        //     "text0": "Je gaat zo verschillende plaatjes zien die je naar links of naar rechts moet verplaatsen. Doe dit zo snel mogelijk en probeer zo min mogelijk fouten te maken. Mocht je toch een foutje maken, geen probleem! Je mag het dan nog een keer proberen.",
+        //     "text1": "Hieronder zie je plaatjes van een programmeur. Deze ga je zo naar links verplaatsen. Dit doe je door op de ‘e’ te tikken op het toetsenbord.",
+        //     "text2": "Ook ga je plaatjes zien van een schrijver. Deze ga je zo naar rechts verplaatsen. Dit doe je door op de ‘i’ te tikken op het toetsenbord.",
+        //     "q_type": "finish",
+        //     "text3": "Zet je linker wijsvinger op de ‘e’, en je rechter wijsvinger op de ‘i’, zo kun je snel plaatjes verplaatsen! Ben je er klaar voor? Druk dan op volgende.",
+        //     "images0": [
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276505/Profession/profession_programmer_1_c6pvby.png",
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_programmer_2_rbcfov.png",
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_programmer_3_cjwqsw.png",
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_programmer_4_xbviey.png"
+        //     ],
+        //     "images1": [
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_writer_1_o8xzue.png",
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276504/Profession/profession_writer_2_mmpdnx.png",
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276505/Profession/profession_writer_3_qyl8aq.png",
+        //         "https://res.cloudinary.com/hctr0xmqp/image/upload/v1591276505/Profession/profession_writer_4_tbvw0b.png"
+        //     ]
+        // }
         {
             "q_type":"finish",
             "title":"Einde",
